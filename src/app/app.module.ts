5435653324dfec8ff02cd05e6891c86f635540b4
adekams <kifademi@gmail.com>
2021-09-25 13:24:02 +0100

    toastr notification installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SuiModule } from 'ng2-semantic-ui';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SuiModule],
+  declarations: [AppComponent, HomepageComponent, CustomButtonComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SuiModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
